b9f80388b35be4ef65e2e614fb4664c0de9a8359
rickysullivan-gallagher <ricky.sullivan@gallagher.com>
2024-02-22 11:18:45 +1300

    Add option for format.

--- index.js
+++ index.js
@@ -24,15 +24,33 @@ program
   .description('CLI to predict files types')
   .version('0.0.1', '-v, --version');
 
-program
+  program
   .command('predict')
   .description('Predict the type of files')
   .argument('<files...>', 'files to predict')
   .option('-p, --pretty', 'Pretty print the output')
+  .option('--format <format>', 'Output format (json or text)', 'json') // Default format is json
   .action(async (files, options) => {
     const predictions = await Promise.all(files.map(makePrediction));
-    const output = options.pretty ? JSON.stringify(predictions, null, 2) : JSON.stringify(predictions);
-    console.log(output);
+    if (options.format === 'json') {
+      const output = options.pretty ? JSON.stringify(predictions, null, 2) : JSON.stringify(predictions);
+      console.log(output);
+    } else if (options.format === 'text') {
+      predictions.forEach((obj, index) => {
+        console.log(`File: ${obj.file}`);
+        if (obj.error) {
+          console.log(`Error: ${obj.error}`);
+        } else {
+          console.log(`Label: ${obj.prediction.label}`);
+          console.log(`Score: ${obj.prediction.score}`);
+        }
+        if(predictions.length > 1 && index < (predictions.length - 1)) {
+          console.log('---')
+        }
+      });
+    } else {
+      console.error('Invalid format. Please use "json" or "text".');
+    }
   });
 
 program.parse();
